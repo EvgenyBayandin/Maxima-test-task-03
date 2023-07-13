646ef9064526d517e1108f3b60e98de0ea86e918
EvgenyBAyandin <5087081@gmail.com>
2023-07-13 16:43:40 +0300

    Added new function - add new user

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ $('#slider').slick({
 	prevArrow: '<div class="slick-own-btn own-btn-prev"><div><</div>',
 	nextArrow: '<div class="slick-own-btn own-btn-next">></div>',
 });
+
+$(document).ready(function() {
+	$("#open-create-user").click(function(){
+		$("#user-modal").fadeIn(200);
+	});
+
+	$(".close").click(function(){
+		$("#user-modal").fadeOut(200);
+	});
+});
